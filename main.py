3ec0fe92f278c8e4319895dee04eb45d29f3c9c4
1ommy <santiago_bravo@mail.ru>
2022-12-02 11:20:48 +0300

    Create  graphics

--- main.py
+++ main.py
@@ -1,45 +1,90 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+from constants import DATA_FILE_PATH
 from test import Columns
 
 plt.style.use('dark_background')
 
 
-def parseCSV(path: str) -> list:
-    data = [[]] * 12
-    file = open(path, "r")
-    s = file.readline()
+class CreateGraph:
+    def __init__(self):
+        self.data = self.parseCSV(DATA_FILE_PATH)
+        self.time = np.array(Columns(self.data, "Time"))
+        self.velocity = np.array(Columns(self.data, "Velocity"))
+        self.gforce = np.array(Columns(self.data, "Gforce"))
+        self.twr = np.array(Columns(self.data, "TWR"))
+        self.mass = np.array(Columns(self.data, "Mass"))
+        self.altitudeFromTerrain = np.array(Columns(self.data, "AltitudeFromTerrain"))
+        self.downrangeDistance = np.array(Columns(self.data, "DownrangeDistance"))
+        self.latitude = np.array(Columns(self.data, "Latitude"))
+        self.apoapsis = np.array(Columns(self.data, "Apoapsis"))
+        self.periapsis = np.array(Columns(self.data, "Periapsis"))
+        self.orbitalVelocity = np.array(Columns(self.data, "OrbitalVelocity"))
 
-    while s != "":
-        s = file.readline().split(",")
-        if len(s) == 1: break
-        for i in range(0, 10):
-            d = 0 if len(s[i]) == 0 else float(s[i])
-            data[i] = data[i] + [d]
-    return data
+    def parseCSV(self, path: str) -> list:
+        data = [[]] * 12
+        file = open(path, "r")
+        s = file.readline()
+
+        while s != "":
+            s = file.readline().split(",")
+            if len(s) == 1: break
+            for i in range(0, 10):
+                d = 0 if len(s[i]) == 0 else float(s[i])
+                data[i] = data[i] + [d]
+        return data
+
+    def create_graph_velocity_by_mass(self):
+        ax1 = plt.subplot(1, 1, 1)
+        plt.plot(self.mass, self.velocity, label="Скорость от Массы")
+        plt.ylabel("Скорость")
+        plt.xlabel("Масса")
+        plt.title("Скорость от Массы")
+        ax1.grid(color='w', linewidth=0.2)
+        plt.show()
+
+    def create_graph_velocity_by_time(self):
+        ax1 = plt.subplot(1, 1, 1)
+        plt.plot(self.time, self.velocity, label="Скорость от Времени")
+        plt.ylabel("Скорость")
+        plt.xlabel("Время")
+        plt.title("Скорость от Времени")
+        ax1.grid(color='w', linewidth=0.2)
+        plt.show()
+
+    def create_graph_velocity_by_altitude(self):
+        ax1 = plt.subplot(1, 1, 1)
+        plt.plot(self.altitudeFromTerrain, self.velocity, label="Скорость от высоты")
+        plt.ylabel("Скорость")
+        plt.xlabel("Высота")
+        plt.title("Скорость от высоты")
+        ax1.grid(color='w', linewidth=0.2)
+        plt.show()
+
+    def create_graph_altitude_by_time(self):
+        ax1 = plt.subplot(1, 1, 1)
+        plt.plot(self.altitudeFromTerrain, self.time, label="Высота от времени")
+        plt.ylabel("Высота")
+        plt.xlabel("Время")
+        plt.title("Высота от времени")
+        ax1.grid(color='w', linewidth=0.2)
+        plt.show()
+
+    def create_graph_GForce_by_altitude(self):
+        ax1 = plt.subplot(1, 1, 1)
+        plt.plot(self.altitudeFromTerrain, self.gforce, label="Ускорение свободного падения G от высоты")
+        plt.ylabel("Ускорение свободного падения G")
+        plt.xlabel("Высота")
+        plt.title("Ускорение свободного падения G от высоты")
+        ax1.grid(color='w', linewidth=0.2)
+        plt.show()
 
 
-Data = parseCSV("/Users/1ommy/ksp/data.csv")
-
-time = np.array(Columns(Data, "Time"))
-velocity = np.array(Columns(Data, "Velocity"))
-G = np.array(Columns(Data, "Gforce"))
-twr = np.array(Columns(Data, "TWR"))
-mass = np.array(Columns(Data, "Mass"))
-AFT = np.array(Columns(Data, "AltitudeFromTerrain"))
-downrangeDistance = np.array(Columns(Data, "DownrangeDistance"))
-latitude = np.array(Columns(Data, "Latitude"))
-apoapsis = np.array(Columns(Data, "Apoapsis"))
-periapsis = np.array(Columns(Data, "Periapsis"))
-OV = np.array(Columns(Data, "OrbitalVelocity"))
-
-ax1 = plt.subplot(1, 1, 1)
-plt.plot(mass, velocity, label="Velocity")
-plt.ylabel("Velocity")
-plt.xlabel("Mass")
-plt.title("Graph")
-
-# TODO:create class that create graphics for each parameter
-
-ax1.grid(color='w', linewidth=0.2)
-plt.show()
+graph = CreateGraph()
+graph.parseCSV(DATA_FILE_PATH)
+graph.create_graph_velocity_by_mass()
+graph.create_graph_velocity_by_time()
+graph.create_graph_velocity_by_altitude()
+graph.create_graph_altitude_by_time()
+graph.create_graph_GForce_by_altitude()
