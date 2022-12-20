8b24aae5385e85aa5b0d32fa7008de64e4555a1e
1ommy <santiago_bravo@mail.ru>
2022-12-20 23:39:37 +0300

    release candidate

--- calculateFormules.py
+++ calculateFormules.py
@@ -1,35 +1,5 @@
 import math
 
-print("""Выберите формулу для расчета:
-1. Формула Циолковского
-2. Закон всемирного тяготения
-3. Сила реактивной тяги
-4. Уравнение Мещерского
-5. Первая космическая скорость
-6. Вторая космическая скорость""")
-
-try:
-    variant = int(input())
-except ValueError:
-    print("Некорректный ввод")
-    exit()
-
-
-def calculate_ciolkovskiy(impulse, g, Mf, Me):
-    return impulse * g * math.log(Mf / Me)
-
-
-def calculate_universal_gravity_law(G, M1, M2, R):
-    return G * M1 * M2 / R ** 2
-
-
-def calculate_reactive_force(m, boost):
-    return m * boost
-
-
-def calculate_mechersky(m, dv, t):
-    return m * dv / t
-
 
 def calculate_first_space_speed(G, M, R):
     return math.sqrt(G * M / R)
@@ -38,30 +8,31 @@ def calculate_first_space_speed(G, M, R):
 def calculate_second_space_speed(G, M, R):
     return math.sqrt(2 * G * M / R)
 
-if variant == 1:
-    print("Введите импульс, ускорение свободного падения, массу топлива, массу ракеты")
-    impulse, g, Mf, Me = map(float, input().split())
-    print(calculate_ciolkovskiy(impulse, g, Mf, Me))
-elif variant == 2:
-    print(
-        "Введите постоянную Гравитационной постоянной, массу первого тела, массу второго тела, расстояние между телами")
-    G, M1, M2, R = map(float, input().split())
-    print(calculate_universal_gravity_law(G, M1, M2, R))
-elif variant == 3:
-    print("Введите массу ракеты, ускорение ракеты")
-    m, boost = map(float, input().split())
-    print(calculate_reactive_force(m, boost))
-elif variant == 4:
-    print("Введите массу ракеты, скорость ракеты, время полета")
-    m, v, t = map(float, input().split())
-    print(calculate_mechersky(m, v, t))
-elif variant == 5:
-    print("Введите постоянную Гравитационной постоянной, массу тела, радиус орбиты")
-    G, M, R = map(float, input().split())
-    print(calculate_first_space_speed(G, M, R))
-elif variant == 6:
-    print("Введите постоянную Гравитационной постоянной, массу тела, радиус орбиты")
-    G, M, R = map(float, input().split())
-    print(calculate_second_space_speed(G, M, R))
-else:
-    print("Нет такой формулы")
+
+def gravity_law(G, m1, m2, R):
+    return G * m1 * m2 / R ** 2
+
+
+G = 6.67 * 10 ** (-11)
+M_EARTH = 5.97 * 10 ** 24
+R_EARTH = 6400 * 1000
+M_MOON = 7.34 * 10 ** 22
+R_MOON = 1737 * 1000
+
+EARTH_ATMOSHERE_ALTITUDE = 160365.51
+BETWEEN_EARTH_AND_MOON_ALTITUDE = 4598615.06
+MOON_ATMOSHERE_ALTITUDE = 115596.56
+# 2229250.06
+print(f"Первая космическая для Земли: {calculate_first_space_speed(G, M_EARTH, R_EARTH)} м/с")
+print(f"Вторая космическая для Земли: {calculate_second_space_speed(G, M_EARTH, R_EARTH)} м/с")
+print(f"Первая космическая для Луны: {calculate_first_space_speed(G, M_MOON, R_MOON)} м/с")
+print(f"Вторая космическая для Луны: {calculate_second_space_speed(G, M_MOON, R_MOON)} м/с")
+
+print(
+    f"Закон всемирного тяготения в момент выхода на орбиту Земли {gravity_law(G, M_EARTH, 20.88 * 1000, EARTH_ATMOSHERE_ALTITUDE)} кг*м/с^2")
+
+print(
+    f"Закон всемирного тяготения в момент нахождения ракеты между Землей и Луной {gravity_law(G, M_EARTH, 6.89 * 1000, BETWEEN_EARTH_AND_MOON_ALTITUDE)} кг*м/с^2")
+
+print(
+    f"Закон всемирного тяготения в момент нахождения ракеты на орбите Луны {gravity_law(G, M_MOON, 5.89 * 1000, MOON_ATMOSHERE_ALTITUDE)} кг*м/с^2")
